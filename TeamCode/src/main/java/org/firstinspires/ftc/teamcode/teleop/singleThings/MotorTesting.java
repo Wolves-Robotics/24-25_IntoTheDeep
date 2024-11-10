b222c4d15dee1fb5da6e793ada96eeb711283314
Kotas37 <75682348+Kotas37@users.noreply.github.com>
2024-11-10 01:17:16 -0600

    gay men sex have fun haha silly go bang bang
    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/singleThings/MotorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/singleThings/MotorTesting.java
@@ -15,38 +15,50 @@ public class MotorTesting extends OpMode {
         robotHardware = new RobotHardware(hardwareMap);
         robotHardware.setDaemon(true);
         robotHardware.start();
+        robotHardware.setServoPos(Names.intakeArm, 0);
+        robotHardware.setServoPos(Names.intakePivot, 0.03);
+
     }
 
     @Override
     public void loop() {
-//        double x = gamepad1.left_stick_x;
-//        double y = -gamepad1.left_stick_y;
-//        double rot = gamepad1.right_stick_x;
-//
-//        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rot), 1);
-//        double frontLeftPower = (y + x + rot) / denominator;
-//        double backLeftPower = (y - x + rot) / denominator;
-//        double frontRightPower = (y - x - rot) / denominator;
-//        double backRightPower = (y + x - rot) / denominator;
-//
-//        robotHardware.setMotorPower(Names.frontLeft, frontLeftPower);
-//        robotHardware.setMotorPower(Names.frontRight, frontRightPower);
-//        robotHardware.setMotorPower(Names.backLeft, backLeftPower);
-//        robotHardware.setMotorPower(Names.backRight, backRightPower);
-//
-//        telemetry.addData("front left", frontLeftPower);
-//        telemetry.addData("front right", frontRightPower);
-//        telemetry.addData("back left", backLeftPower);
-//        telemetry.addData("back right", backRightPower);
-//        telemetry.update();
+        double x = gamepad1.left_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double rot = gamepad1.right_stick_x;
 
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rot), 1);
+        double frontLeftPower = (y + x + rot) / denominator;
+        double backLeftPower = (y - x + rot) / denominator;
+        double frontRightPower = (y - x - rot) / denominator;
+        double backRightPower = (y + x - rot) / denominator;
+
+        robotHardware.setMotorPower(Names.frontLeft, frontLeftPower);
+        robotHardware.setMotorPower(Names.frontRight, frontRightPower);
+        robotHardware.setMotorPower(Names.backLeft, backLeftPower);
+        robotHardware.setMotorPower(Names.backRight, backRightPower);
+
+        telemetry.addData("front left", frontLeftPower);
+        telemetry.addData("front right", frontRightPower);
+        telemetry.addData("back left", backLeftPower);
+        telemetry.addData("back right", backRightPower);
+        telemetry.update();
+        robotHardware.setMotorPower(Names.slurp,gamepad1.right_trigger - gamepad1.left_trigger);
         if (gamepad1.a) robotHardware.setMotorPower(Names.leftOuttake, 0.25);
         else robotHardware.setMotorPower(Names.leftOuttake, 0);
         if (gamepad1.b) robotHardware.setMotorPower(Names.rightOuttake, 0.25);
         else robotHardware.setMotorPower(Names.rightOuttake, 0);
         if (gamepad1.x) robotHardware.setMotorPower(Names.intakeExtendo, 1);
+        else if(gamepad1.y) robotHardware.setMotorPower(Names.intakeExtendo, -1);
         else robotHardware.setMotorPower(Names.intakeExtendo, 0);
-        if (gamepad1.y) robotHardware.setMotorPower(Names.slurp, 0.25);
-        else robotHardware.setMotorPower(Names.slurp, 0);
+        if(gamepad1.dpad_down){
+            robotHardware.setServoPos(Names.intakeArm, 0.73);
+            robotHardware.setServoPos(Names.intakePivot, 0.3);
+        }
+        if(gamepad1.dpad_up){
+            robotHardware.setServoPos(Names.intakeArm, 0);
+            robotHardware.setServoPos(Names.intakePivot, 0);
+        }
+        if(gamepad1.right_bumper) robotHardware.setServoPos(Names.door, 0.4);
+        else robotHardware.setServoPos(Names.door, 0.07);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/singleThings/ServoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/singleThings/ServoTesting.java
@@ -9,24 +9,24 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 @Config
 @TeleOp
 public class ServoTesting extends OpMode {
-    ServoImplEx intakePitch, armPitch, outtakeArm, outtakePivot;
+    ServoImplEx intakePivot, intakeArm, outtakeArm, outtakePivot;
     public static double p1 = 0, p2 = 0, p3 = 0, p4 = 0;
     public static boolean b1 = false, b2 = false, b3 = false, b4 = false;
 
     @Override
     public void init() {
-        intakePitch = hardwareMap.get(ServoImplEx.class, "intakePitch");
-        armPitch = hardwareMap.get(ServoImplEx.class, "armPitch");
+        intakePivot = hardwareMap.get(ServoImplEx.class, "intakePivot");
+        intakeArm = hardwareMap.get(ServoImplEx.class, "intakeArm");
         outtakeArm = hardwareMap.get(ServoImplEx.class, "outtakeArm");
         outtakePivot = hardwareMap.get(ServoImplEx.class, "outtakePivot");
     }
 
     @Override
     public void loop() {
-        intakePitch.setPosition(p1);
-        intakePitch.setDirection(b1? Servo.Direction.REVERSE: Servo.Direction.FORWARD);
-        armPitch.setPosition(p2);
-        armPitch.setDirection(b2? Servo.Direction.REVERSE: Servo.Direction.FORWARD);
+        intakePivot.setPosition(p1);
+        intakePivot.setDirection(b1? Servo.Direction.REVERSE: Servo.Direction.FORWARD);
+        intakeArm.setPosition(p2);
+        intakeArm.setDirection(b2? Servo.Direction.REVERSE: Servo.Direction.FORWARD);
         outtakeArm.setPosition(p3);
         outtakeArm.setDirection(b3? Servo.Direction.REVERSE: Servo.Direction.FORWARD);
         outtakePivot.setPosition(p4);
